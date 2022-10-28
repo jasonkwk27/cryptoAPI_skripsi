81f07cc6a53c753a9e139b5d1a404e371adc3f6b
jasonkwk27 <113696743+jasonkwk27@users.noreply.github.com>
2022-10-28 18:49:18 +0700

    quality of life for input
    list changes everytime input is changed
    hide list when input is un-focused

--- client/src/views/user/TradingHistoryView.vue
+++ client/src/views/user/TradingHistoryView.vue
@@ -121,10 +121,10 @@
                 <div class = "flex rounded-lg">  
                     <div v-auto-animate class = "mx-5 my-3 rounded-lg">
                         <div class = "flex items-center bg-[#0F4C75] outline-white outline-1 hover:outline rounded-lg">
-                            <input type ="text" class = "py-3 px-5  bg-[#0F4C75] text-[#BBE1FA] rounded-lg focus:outline-none" v-model = "symbol_input"  @click ="symbol_clicked = !symbol_clicked" placeholder = "Select a pair">
+                            <input type ="text" class = "py-3 px-5  bg-[#0F4C75] text-[#BBE1FA] rounded-lg focus:outline-none" v-model = "symbol_input" @input="symbol_inputChanged" @blur = "symbol_clicked = false" @click ="symbol_clicked = !symbol_clicked" placeholder = "Select a pair">
                             <a href = "#">
                             <svg xmlns="http://www.w3.org/2000/svg" id="Bold" viewBox="0 0 24 24" width="15" height="15" class = "m-3" v-if="!symbol_clicked"  @click ="symbol_clicked = !symbol_clicked"><path d="M1.51,6.079a1.492,1.492,0,0,1,1.06.44l7.673,7.672a2.5,2.5,0,0,0,3.536,0L21.44,6.529A1.5,1.5,0,1,1,23.561,8.65L15.9,16.312a5.505,5.505,0,0,1-7.778,0L.449,8.64A1.5,1.5,0,0,1,1.51,6.079Z" fill="#BBE1FA"/></svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" v-else class = "m-3" viewBox="0 0 513.749 513.749" style="enable-background:new 0 0 513.749 513.749;" xml:space="preserve" width="15" height="15" @click = "symbol_inputChanged">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" v-else class = "m-3" viewBox="0 0 513.749 513.749" style="enable-background:new 0 0 513.749 513.749;" xml:space="preserve" width="15" height="15">
                             <g>
                                 <path d="M504.352,459.061l-99.435-99.477c74.402-99.427,54.115-240.344-45.312-314.746S119.261-9.277,44.859,90.15   S-9.256,330.494,90.171,404.896c79.868,59.766,189.565,59.766,269.434,0l99.477,99.477c12.501,12.501,32.769,12.501,45.269,0   c12.501-12.501,12.501-32.769,0-45.269L504.352,459.061z M225.717,385.696c-88.366,0-160-71.634-160-160s71.634-160,160-160   s160,71.634,160,160C385.623,314.022,314.044,385.602,225.717,385.696z"  fill="#BBE1FA"/>
                             </g>
